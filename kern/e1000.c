2167af033c1d38ac761e0435250240b0703570f8
WANG Siyuan <wangsiyuanbuaa@gmail.com>
2015-06-22 16:49:49 +0800

    Lab 6, Exercise 3. make grade Timeout!

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,3 +1,10 @@
 #include <kern/e1000.h>
 
 // LAB 6: Your driver code here
+#include <inc/stdio.h>
+int e1000_attach (struct pci_func *pcif)
+{
+	cprintf("e1000_attach begin\n");
+	pci_func_enable(pcif);
+	return 0;
+}
